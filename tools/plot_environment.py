743234dd685331e0fa2b48fdb22062904e9eddf3
llwyd <tom@llwyd.io>
2024-10-07 20:38:20 +0100

    better plotting

--- tools/plot_environment.py
+++ tools/plot_environment.py
@@ -10,11 +10,22 @@ metadata = db.MetaData()
 
 environment_data = db.Table('environment_data', metadata, autoload_with=engine)
 
-query = environment_data.select().where(environment_data.columns.device_id == "bedroom")
+#query = environment_data.select().where(environment_data.columns.device_id == "bedroom")
 
+query = environment_data.select()
 query_output = conn.execute(query)
 
 df = pd.DataFrame(query_output.fetchall())
-plt.plot(np.float32(df['temperature']))
+
+df['temperature'] = df['temperature'].astype(float)
+df['humidity'] = df['humidity'].astype(float)
+
+nodes = df.device_id.unique()
+
+for node in nodes:
+    data = df[df['device_id'] == node]
+    plt.plot(np.float32(data['temperature']))
+
+plt.legend(nodes)
 plt.show()
 
