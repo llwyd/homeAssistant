9a4dbd4ea400485bdbaa5888ed96c7dd9815660c
llwyd <tom@llwyd.io>
2024-10-10 12:39:20 +0100

    successful graph display

--- homepage/app/routes.py
+++ homepage/app/routes.py
@@ -3,17 +3,61 @@ from sqlalchemy import or_
 from app import app, db
 from app.models import EnvironmentData
 import datetime as dt
+import pandas as pd
+from matplotlib.figure import Figure
+import base64
+from io import BytesIO
+import numpy as np
 
-@app.route("/")
-def index():
-    return "Hello World"
-
-@app.route('/dump')
-def dump_data():
+def get_todays_data():
     todays_date            = dt.datetime.now().strftime('%Y-%m-%d')
     yesterdays_date        = (dt.datetime.now() - dt.timedelta(days=1)).strftime('%Y-%m-%d') 
     yesterdays_time        = (dt.datetime.now() - dt.timedelta(days=1)) 
    
     with app.app_context():
         todays_data = db.session.query(EnvironmentData).filter( or_(EnvironmentData.datestamp == todays_date, EnvironmentData.datestamp == yesterdays_date)).all() 
+    return todays_data
+
+def generate_data_graph(todays_data):
+    fig = Figure()
+    ax = fig.subplots()
+    df = pd.DataFrame([(data.device_id, data.datestamp, data.timestamp, data.temperature, data.humidity) for data in todays_data], columns=['device_id','datestamp','timestamp','temperature','humidity'])
+
+    df['temperature'] = df['temperature'].astype(float)
+    df['humidity'] = df['humidity'].astype(float)
+    nodes = df.device_id.unique()
+
+    for node in nodes:
+        data = df.copy()
+        data = data[data['device_id'] == node]
+        data['datetime'] = data['datestamp'] + " " + data['timestamp'] 
+        d_time = [dt.datetime.strptime(date,'%Y-%m-%d %H:%M') for date in data['datetime']]
+        ax.plot(d_time, np.float32(data['temperature']),label=node)
+    
+    ax.set_title('Temperature')
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Degrees (C)')
+    ax.tick_params('x', labelrotation=45)
+    ax.legend()
+
+    buf = BytesIO()
+    fig.savefig(buf, format='png')
+    return base64.b64encode(buf.getbuffer()).decode("ascii")
+
+@app.route("/")
+def index():
+    
+    todays_data = get_todays_data()
+    temperature_graph = generate_data_graph(todays_data)    
+    last_update = dt.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
+    return render_template('index.html',
+                           graph=temperature_graph,
+                           last_update=last_update,
+                           site_version="N/A",
+                           uptime="N/A",
+                           environment=[])
+
+@app.route('/dump')
+def dump_data():
+    todays_data = get_todays_data()
     return render_template("raw_data.html", readings=todays_data)
