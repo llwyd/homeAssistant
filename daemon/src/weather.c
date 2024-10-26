5ddb03fc0ecf7053e342b9b0f38c1b42623beae5
llwyd <tom@llwyd.io>
2024-10-26 14:40:43 +0100

    beginning of weather instance

---
+++ daemon/src/weather.c
@@ -0,0 +1,14 @@
+#include "weather.h"
+
+#define SIGNALS(SIG ) \
+    SIG( Tick ) \
+    SIG( MessageReceived ) \
+    SIG( Disconnect ) \
+    SIG( UpdateHomepage ) \
+
+
+extern void Weather_Init(void)
+{
+
+}
+
